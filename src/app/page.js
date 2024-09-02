bb3b89215e5c18b7b269b608cb0124d6749b1b3f
Saikat <saikat25112000@gmail.com>
2024-09-02 11:05:44 +0600

    Project setup

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      
+    Hello car service
+    <button className="btn btn-accent">Accent</button>
+    </main>
+  );
+}
