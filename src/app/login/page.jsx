ce3dde560fcce28ac15d397f171a24062f58dc77
Saikat <saikat25112000@gmail.com>
2024-09-03 23:36:26 +0600

    ERr

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,13 +1,101 @@
+// "use client";
+
+// import React from 'react';
+// import Link from "next/link";
+// import { signIn } from "next-auth/react";
+// import { useRouter, useSearchParams } from "next/navigation";
+// import SocialSignin from "@/components/shared/SocialSignin";
+
+// const Page = () => {
+//   const router = useRouter();
+//   const searchParams = useSearchParams();
+//   const path = searchParams.get("redirect");
+
+//   const handleLogin = async (event) => {
+//     event.preventDefault();
+//     const email = event.target.email.value;
+//     const password = event.target.password.value;
+    
+//     try {
+//       const result = await signIn("credentials", {
+//         email,
+//         password,
+//         redirect: false,
+//       });
+
+//       if (result?.ok) {
+//         router.push(path || "/");
+//       } else {
+//         // Handle login errors
+//         console.error("Login failed:", result?.error);
+//       }
+//     } catch (error) {
+//       console.error("Error during login:", error);
+//     }
+//   };
+
+//   return (
+//     <div className="flex items-center justify-center min-h-screen">
+//       <div className="w-full max-w-sm p-8 bg-white border rounded-lg shadow-md">
+//         <h6 className="text-2xl font-semibold text-primary text-center mb-6">
+//           Sign In
+//         </h6>
+//         <form onSubmit={handleLogin}>
+//           <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+//             Email
+//           </label>
+//           <input
+//             type="email"
+//             name="email"
+//             placeholder="your email"
+//             className="mt-2 mb-4 w-full input input-bordered"
+//             required
+//           />
+//           <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+//             Password
+//           </label>
+//           <input
+//             type="password"
+//             name="password"
+//             placeholder="your password"
+//             className="mt-2 mb-4 w-full input input-bordered"
+//             required
+//           />
+//           <button
+//             type="submit"
+//             className="w-full btn btn-primary text-white mt-4 text-lg"
+//           >
+//             Sign In
+//           </button>
+//         </form>
+//         <div className="text-center mt-6">
+//           <h6 className="mb-4">or sign in with</h6>
+//           <SocialSignin />
+//           <h6 className="mt-4">
+//             Not have an account?{" "}
+//             <Link className="text-primary font-semibold" href={"/signup"}>
+//               Sign Up
+//             </Link>
+//           </h6>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Page;
+
 "use client";
 
-import React from 'react';
 import Link from "next/link";
-import { signIn } from "next-auth/react";
-import { useRouter, useSearchParams } from "next/navigation";
-import SocialSignin from "@/components/shared/SocialSignin";
+import React  from "react";
+import { signIn, useSession } from "next-auth/react";
 
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import SocialSignin from "@/components/shared/SocialSignin";
 const Page = () => {
   const router = useRouter();
+  const session = useSession();
   const searchParams = useSearchParams();
   const path = searchParams.get("redirect");
 
@@ -15,71 +103,59 @@ const Page = () => {
     event.preventDefault();
     const email = event.target.email.value;
     const password = event.target.password.value;
-    
-    try {
-      const result = await signIn("credentials", {
-        email,
-        password,
-        redirect: false,
-      });
-
-      if (result?.ok) {
-        router.push(path || "/");
-      } else {
-        // Handle login errors
-        console.error("Login failed:", result?.error);
-      }
-    } catch (error) {
-      console.error("Error during login:", error);
-    }
+    const resp = await signIn("credentials", {
+      email,
+      password,
+      redirect: true,
+      callbackUrl: path ? path : "/",
+    });
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="w-full max-w-sm p-8 bg-white border rounded-lg shadow-md">
-        <h6 className="text-2xl font-semibold text-primary text-center mb-6">
-          Sign In
-        </h6>
-        <form onSubmit={handleLogin}>
-          <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-            Email
-          </label>
-          <input
-            type="email"
-            name="email"
-            placeholder="your email"
-            className="mt-2 mb-4 w-full input input-bordered"
-            required
-          />
-          <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-            Password
-          </label>
-          <input
-            type="password"
-            name="password"
-            placeholder="your password"
-            className="mt-2 mb-4 w-full input input-bordered"
-            required
-          />
-          <button
-            type="submit"
-            className="w-full btn btn-primary text-white mt-4 text-lg"
-          >
-            Sign In
-          </button>
-        </form>
-        <div className="text-center mt-6">
-          <h6 className="mb-4">or sign in with</h6>
-          <SocialSignin />
-          <h6 className="mt-4">
-            Not have an account?{" "}
-            <Link className="text-primary font-semibold" href={"/signup"}>
-              Sign Up
-            </Link>
-          </h6>
+      <div className="container px-24 mx-auto py-24">
+        <div className="grid grid-cols-2 gap-12 items-center">
+          
+          <div className="border-2 p-12">
+            <h6 className="text-3xl font-semibold text-primary text-center mb-12">
+              Sign In
+            </h6>
+            <form onSubmit={handleLogin} action="">
+              <label htmlFor="email">Email</label> <br />
+              <input
+                type="text"
+                name="email"
+                placeholder="your email"
+                className="mt-3 w-full input input-bordered"
+              />
+              <br /> <br />
+              <label htmlFor="password">Password</label> <br />
+              <input
+                type="password"
+                name="password"
+                placeholder="your password"
+                className="w-full mt-3 input input-bordered"
+              />
+              <br />
+              <button
+                type="submit"
+                className="w-full btn btn-primary mt-12 text-lg"
+              >
+                Sign In
+              </button>
+            </form>
+            <div>
+              <h6 className="my-12 text-center">or sign in with</h6>
+              <SocialSignin />
+              <h6 className="my-12 text-center">
+                not have account ?{" "}
+                <Link className="text-primary font-semibold" href={"/signup"}>
+                  Sign Up
+                </Link>
+              </h6>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
   );
 };
 
--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,163 +1,79 @@
-// "use client";
-
-// import SocialSignin from "@/components/shared/SocialSignin";
-// import Link from "next/link";
-// import React from "react";
-// import { BsGithub, BsGoogle } from "react-icons/bs";
-
-// const SignUpPage = () => {
-//   const handleSignUp = async (event) => {
-//     event.preventDefault();
-//     const newUser = {
-//         name: event.target.name.value,
-//         email: event.target.email.value,
-//         password: event.target.password.value,
-//     };
-//     const resp = await fetch ("http://localhost:3000/signup/api", {
-//         method: "POST",
-//         body: JSON.stringify(newUser),
-//         headers: {
-//           "content-type": "application/json"
-//         }
-//       })
-//       if (resp.status === 200) {
-//         event.target.reset();
-//       }
-//   };
-
-//   return (
-//     <div className="flex items-center justify-center min-h-screen ">
-//       <div className="w-full max-w-sm p-6 bg-white border rounded shadow-md">
-//         <h6 className="text-xl font-semibold text-primary text-center mb-4">
-//           Sign Up
-//         </h6>
-//         <form onSubmit={handleSignUp}>
-//           <input
-//             type="text"
-//             name="name"
-//             placeholder="Name"
-//             className="mb-3 w-full input input-bordered"
-//           />
-//           <input
-//             type="text"
-//             name="email"
-//             placeholder="Email"
-//             className="mb-3 w-full input input-bordered"
-//           />
-//           <input
-//             type="password"
-//             name="password"
-//             placeholder="Password"
-//             className="mb-3 w-full input input-bordered"
-//           />
-//           <button
-//             type="submit"
-//             className="w-full btn btn-primary text-white"
-//           >
-//             Sign Up
-//           </button>
-//         </form>
-//         <div className="text-center mt-4">
-//             <SocialSignin></SocialSignin>
-//         {/* <div className="flex items-center justify-center space-x-3 mb-4">
-//             <button className="btn p-5 flex items-center justify-center text-primary text-xl"><BsGithub/></button>
-//             <button className="btn p-5 flex items-center justify-center text-green-500 text-xl"><BsGoogle/></button>
-//           </div> */}
-//           <h6>
-//             Have an account?{" "}
-//             <Link className="text-primary font-semibold" href="/login">
-//               Sign In
-//             </Link>
-//           </h6>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default SignUpPage;
-
-
 "use client";
-
 import SocialSignin from "@/components/shared/SocialSignin";
+
 import Link from "next/link";
-import React from "react";
-import { BsGithub, BsGoogle } from "react-icons/bs";
+import React, { Suspense } from "react";
 
 const SignUpPage = () => {
   const handleSignUp = async (event) => {
     event.preventDefault();
     const newUser = {
-        name: event.target.name.value,
-        email: event.target.email.value,
-        password: event.target.password.value,
+      name: event.target.name.value,
+      email: event.target.email.value,
+      password: event.target.password.value,
     };
-
-    try {
-      const resp = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/signup/api`, {
-        method: "POST",
-        body: JSON.stringify(newUser),
-        headers: {
-          "Content-Type": "application/json"
-        }
-      });
-
-      if (resp.status === 200) {
-        event.target.reset();
-        // Optionally redirect to login page or show a success message
-      } else {
-        // Handle errors or show user feedback
-        console.error("Sign up failed:", await resp.text());
-      }
-    } catch (error) {
-      console.error("Error during sign up:", error);
+    const resp = await fetch("https://car-doctor-pro-nine.vercel.app/signup/api", {
+      method: "POST",
+      body: JSON.stringify(newUser),
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+    if (resp.status === 200) {
+      event.target.reset();
     }
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen ">
-      <div className="w-full max-w-sm p-6 bg-white border rounded shadow-md">
-        <h6 className="text-xl font-semibold text-primary text-center mb-4">
-          Sign Up
-        </h6>
-        <form onSubmit={handleSignUp}>
-          <input
-            type="text"
-            name="name"
-            placeholder="Name"
-            className="mb-3 w-full input input-bordered"
-            required
-          />
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            className="mb-3 w-full input input-bordered"
-            required
-          />
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            className="mb-3 w-full input input-bordered"
-            required
-          />
-          <button
-            type="submit"
-            className="w-full btn btn-primary text-white"
-          >
+    <div className="container px-24 mx-auto py-24">
+      <div className="grid grid-cols-2 gap-12 items-center">
+        
+        <div className="border-2 p-12">
+          <h6 className="text-3xl font-semibold text-primary text-center mb-12">
             Sign Up
-          </button>
-        </form>
-        <div className="text-center mt-4">
-            <SocialSignin />
-          <h6>
-            Have an account?{" "}
-            <Link className="text-primary font-semibold" href="/login">
-              Sign In
-            </Link>
           </h6>
+          <form onSubmit={handleSignUp} action="">
+            <label htmlFor="email">Name</label> <br />
+            <input
+              type="text"
+              name="name"
+              placeholder="your name"
+              className="mt-3 w-full input input-bordered"
+            />
+            <br /> <br />
+            <label htmlFor="email">Email</label> <br />
+            <input
+              type="text"
+              name="email"
+              placeholder="your email"
+              className="mt-3 w-full input input-bordered"
+            />
+            <br /> <br />
+            <label htmlFor="password">Password</label> <br />
+            <input
+              type="password"
+              name="password"
+              placeholder="your password"
+              className="w-full mt-3 input input-bordered"
+            />
+            <br />
+            <button
+              type="submit"
+              className="w-full btn btn-primary mt-12 text-lg"
+            >
+              Sign Up
+            </button>
+          </form>
+          <div>
+            <h6 className="my-12 text-center">or sign in with</h6>
+            <SocialSignin />
+            <h6 className="my-12 text-center">
+              Already have account ?{" "}
+              <Link className="text-primary font-semibold" href={"/login"}>
+                Sign In
+              </Link>
+            </h6>
+          </div>
         </div>
       </div>
     </div>
