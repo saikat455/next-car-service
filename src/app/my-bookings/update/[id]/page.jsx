2334a2785e3b8cb548159f446d7be15741c24df0
Saikat <saikat25112000@gmail.com>
2024-09-03 20:59:01 +0600

    Trying deploying

--- src/app/my-bookings/update/[id]/page.jsx
+++ src/app/my-bookings/update/[id]/page.jsx
@@ -1,56 +1,53 @@
 "use client";
-import { getServicesDetails } from "@/services/getServices";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
-import { ToastContainer, toast } from "react-toastify";
+import { toast } from "react-toastify";
 
-const Checkout = ({ params }) => {
-    const {data} = useSession();
-  const [ service, setService ] = useState({});
-  const loadService = async () => {
-    const details = await getServicesDetails(params.id);
-    setService(details.service);
+const Page = ({ params }) => {
+  const { data } = useSession();
+  const [booking, setBooking] = useState([]);
+
+  const loadBooking = async () => {
+    const bookingDetail = await fetch(
+      `https://car-doctor-pro-nine.vercel.app/my-bookings/api/booking/${params.id}`
+    );
+    const data = await bookingDetail.json();
+    setBooking(data.data);
   };
-  const { _id, title, description, img, price, facility } = service || {};
 
-  const handleBooking = async (event) => {
+  const handleUpdateBooking = async (event) => {
     event.preventDefault();
-    const newBooking = { 
-        email : data?.user?.email,
-        name : data?.user?.name,
-        address : event.target.address.value,
-        phone : event.target.phone.value,
-        date : event.target.date.value,
-        serviceTitle : title,
-        serviceID : _id,
-        price : price,
+    const updatedBooking = {
+      date: event.target.date.value,
+      phone: event.target.phone.value,
+      address: event.target.address.value,
+    };
+    const resp = await fetch(
+      `https://car-doctor-pro-nine.vercel.app/my-bookings/api/booking/${params.id}`,
+      {
+        method: "PATCH",
+        body: JSON.stringify(updatedBooking),
+        headers: {
+          "content-type": "application/json",
+        },
+      }
+    );
+    if(resp.status === 200) {
+      toast.success("Updated Successfully")
     }
-
-    const resp = await fetch('https://car-doctor-pro-nine.vercel.app/checkout/api/new-booking', {
-        method: 'POST',
-        body: JSON.stringify(newBooking),
-        headers : {
-            "content-type" : "application/json"
-        }
-    })
-    const response =await resp?.json()
-    toast.success(response?.message)
-    event.target.reset()
-
   };
 
   useEffect(() => {
-    loadService()
-  },[params, loadService])
+    loadBooking();
+  }, [params]);
 
   return (
     <div className="container mx-auto">
-      <ToastContainer/>
       <div className="relative  h-72">
         <Image
           className="absolute h-72 w-full left-0 top-0 object-cover"
-          src={img}
+          src={""}
           alt="service"
           width={1920}
           height={1080}
@@ -58,31 +55,41 @@ const Checkout = ({ params }) => {
         />
         <div className="absolute h-full left-0 top-0 flex items-center justify-center bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] ">
           <h1 className="text-white text-3xl font-bold flex justify-center items-center ml-8">
-            Checkout {title}
+            Update Booking
           </h1>
         </div>
       </div>
       <div className="my-12 bg-slate-300 p-12">
-        <form onSubmit={handleBooking}>
+        <form onSubmit={handleUpdateBooking}>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Name</span>
               </label>
-              <input defaultValue={data?.user?.name}  type="text" name="name" className="input input-bordered" />
+              <input
+                defaultValue={data?.user?.name}
+                type="text"
+                name="name"
+                className="input input-bordered"
+              />
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Date</span>
               </label>
-              <input defaultValue={new Date().getDate()} type="date" name="date" className="input input-bordered" />
+              <input
+                defaultValue={booking.date}
+                type="date"
+                name="date"
+                className="input input-bordered"
+              />
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
               <input
-              defaultValue={data?.user?.email}
+                defaultValue={data?.user?.email}
                 type="text"
                 name="email"
                 placeholder="email"
@@ -94,8 +101,8 @@ const Checkout = ({ params }) => {
                 <span className="label-text">Due amount</span>
               </label>
               <input
-              defaultValue={price}
-              readOnly
+                defaultValue={booking.price}
+                readOnly
                 type="text"
                 name="price"
                 className="input input-bordered"
@@ -106,7 +113,8 @@ const Checkout = ({ params }) => {
                 <span className="label-text">Phone</span>
               </label>
               <input
-              required
+                defaultValue={booking.phone}
+                required
                 type="text"
                 name="phone"
                 placeholder="Your Phone"
@@ -118,6 +126,7 @@ const Checkout = ({ params }) => {
                 <span className="label-text">Present Address</span>
               </label>
               <input
+                defaultValue={booking.address}
                 type="text"
                 name="address"
                 placeholder="Your Address"
@@ -138,4 +147,4 @@ const Checkout = ({ params }) => {
   );
 };
 
-export default Checkout;
+export default Page;
