2a86b1a2654e8671e025a317721207b0ab919dd9
Saikat <saikat25112000@gmail.com>
2024-09-03 22:54:11 +0600

    error

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -3,11 +3,11 @@
 import Link from "next/link";
 import React from "react";
 import { signIn, useSession } from "next-auth/react";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import SocialSignin from "@/components/shared/SocialSignin";
-import SuspenseBoundary from "@/components/SuspenseBoundary";
 
 const Page = () => {
+  const router = useRouter();
   const searchParams = useSearchParams();
   const path = searchParams.get("redirect");
 
@@ -15,66 +15,67 @@ const Page = () => {
     event.preventDefault();
     const email = event.target.email.value;
     const password = event.target.password.value;
-    await signIn("credentials", {
+    const result = await signIn("credentials", {
       email,
       password,
-      redirect: true,
-      callbackUrl: path ? path : "/",
+      redirect: false, // Set to false to handle redirection manually
     });
+
+    if (result?.ok) {
+      router.push(path || "/");
+    }
   };
 
   return (
-    <SuspenseBoundary>
-      <div className="flex items-center justify-center min-h-screen">
-        <div className="w-full max-w-sm p-8 bg-white border rounded-lg shadow-md">
-          <h6 className="text-2xl font-semibold text-primary text-center mb-6">
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="w-full max-w-sm p-8 bg-white border rounded-lg shadow-md">
+        <h6 className="text-2xl font-semibold text-primary text-center mb-6">
+          Sign In
+        </h6>
+        <form onSubmit={handleLogin}>
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Email
+          </label>
+          <input
+            type="text"
+            name="email"
+            placeholder="your email"
+            className="mt-2 mb-4 w-full input input-bordered"
+          />
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            placeholder="your password"
+            className="mt-2 mb-4 w-full input input-bordered"
+          />
+          <button
+            type="submit"
+            className="w-full btn btn-primary text-white mt-4 text-lg"
+          >
             Sign In
+          </button>
+        </form>
+        <div className="text-center mt-6">
+          <h6 className="mb-4">or sign in with</h6>
+          <SocialSignin />
+          <h6 className="mt-4">
+            Not have an account?{" "}
+            <Link className="text-primary font-semibold" href={"/signup"}>
+              Sign Up
+            </Link>
           </h6>
-          <form onSubmit={handleLogin}>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Email
-            </label>
-            <input
-              type="text"
-              name="email"
-              placeholder="your email"
-              className="mt-2 mb-4 w-full input input-bordered"
-            />
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              name="password"
-              placeholder="your password"
-              className="mt-2 mb-4 w-full input input-bordered"
-            />
-            <button
-              type="submit"
-              className="w-full btn btn-primary text-white mt-4 text-lg"
-            >
-              Sign In
-            </button>
-          </form>
-          <div className="text-center mt-6">
-            <h6 className="mb-4">or sign in with</h6>
-            <SocialSignin />
-            <h6 className="mt-4">
-              Have an account?{" "}
-              <Link className="text-primary font-semibold" href={"/signup"}>
-                Sign Up
-              </Link>
-            </h6>
-          </div>
         </div>
       </div>
-    </SuspenseBoundary>
+    </div>
   );
 };
 
